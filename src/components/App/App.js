63a2476f929f34eb4f65cfde56b869adb088ce1d
Andrey Avtushenko <palaxius@mail.ru>
2020-09-10 20:25:14 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Building a Spotify clone!</h1>
+
+        {/* Spotify Logo */}
+        {/* Login with spotify button */}
+
+    </div>
+  );
+}
+
+export default App;
+
+// Client ID 540455896e7542ce8a24b8a0ec3f9bb0
+
+// Client Secret a0881edf7ad14b9ca6a338940fcc05ba
